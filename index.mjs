35ad312b42c1aa61ac2a98f14a8a1f5a4665a262
Sean <seanalunni@gmail.com>
2023-01-05 23:41:14 +0100

    1.2.0

--- index.mjs
+++ index.mjs
@@ -1,19 +1,57 @@
 
+import common from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import solidStartNode from "solid-start-node";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 import { copyFileSync } from "fs";
-import { join } from "path";
+import { rollup } from "rollup";
 
-export default function() {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default function () {
+  return {
     // Extends the standard implementation
-    return new class extends function () { return solidStartNode(); } {
-        async build() {
-            // Tries to be as dependent as possible from the standard implementation in order to be as compatible as possible with new versions
-            await super.build();
+    ...solidStartNode(),
 
-            // Uses the custom version of "entry.js"
-            const from = import.meta.resolve("./entry.js");
-            const to = join(config.root, ".solid", "server", "server.js");
-            copyFileSync(from, to);
-        }
-    };
+    // Overrides the "build()" method
+    async build(config, builder) {
+      const ssrExternal = config?.ssr?.external || [];
+      const dist = join(config.root, "dist"); 
+      const pub = join(dist, "public");
+      const ser = join(config.root, ".solid", "server");
+
+      if (!config.solidOptions.ssr) await builder.spaClient(pub);
+      else if (config.solidOptions.islands) await builder.islandsClient(pub);
+      else await builder.client(pub);
+      await builder.server(ser);
+
+      copyFileSync(join(ser, "entry-server.js"), join(ser, "handler.js"));
+      copyFileSync(join(__dirname, "entry.js"), join(ser, "server.js")); // ← What I changed
+
+      builder.debug(`bundling server with rollup`);
+
+      const bundle = await rollup({
+        input: join(ser, "server.js"),
+        plugins: [
+          json(),
+          nodeResolve({
+            preferBuiltins: true,
+            exportConditions: [ "node", "solid" ]
+          }),
+          common()
+        ],
+        external: [ "undici", "stream/web", ...ssrExternal ]
+      });
+
+      // or write the bundle to disk
+      await bundle.write({ format: "esm", dir: dist });
+
+      // closes the bundle
+      await bundle.close();
+
+      builder.debug(`bundling server done`);
+    }
+  }
 }
